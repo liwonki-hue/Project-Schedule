20689f6865fded321f664e66e45afe35ea3d03b0
liwonki-hue <liwonki@gmail.com>
2026-04-21 21:44:25 +0900

    Elaborate AS/HW description to supply & return of Hot Water

--- app.py
+++ app.py
@@ -262,7 +262,7 @@ ESSENTIAL_SYSTEMS = [
     {"System": "Fuel Gas & FGH System", "Area": "MB / GT / FGH", "Description": "Supply Fuel Gas to GT", "DI": 2480, "EA": 610, "Criticality": "Mandatory", "Equipment": "FGSS System, Performance Heater & Final Filter", "Remark": ""},
     {"System": "Closed Cooling Water (CCW)", "Area": "MB / PR / PH#1 / FFC#1", "Description": "Supply & Return of CCW for equipment cooling down", "DI": 4120, "EA": 980, "Criticality": "Mandatory", "Equipment": "PH#1 & FFC#1 Integrated", "Remark": ""},
     {"System": "Instrument Air (IA)", "Area": "All Areas", "Description": "Supply Instrument Air to all plant", "DI": 1150, "EA": 290, "Criticality": "Operation", "Equipment": "Air Compressors, IA Dryers", "Remark": ""},
-    {"System": "Aux. Steam & Hot Water (AS/HW)", "Area": "PR#3/4 / MB", "Description": "Anti-Icing path.", "DI": 2650, "EA": 660, "Criticality": "Mandatory", "Equipment": "Aux. Boiler, Hot Water Supply Pump, Heat Exchanger", "Remark": ""},
+    {"System": "Aux. Steam & Hot Water (AS/HW)", "Area": "PR#3/4 / MB", "Description": "supply & return of Hot Water for GT Anti-Icing", "DI": 2650, "EA": 660, "Criticality": "Mandatory", "Equipment": "Aux. Boiler, Hot Water Supply Pump, Heat Exchanger", "Remark": ""},
     {"System": "Nitrogen System (N2)", "Area": "GT Area / PR", "Description": "Purging and inerting.", "DI": 320, "EA": 90, "Criticality": "Mandatory", "Equipment": "N2 Storage Tank, N2 Supply System", "Remark": ""},
     {"System": "GT MISC (Vents)", "Area": "Main Building", "Description": "Gas vent & drainage.", "DI": 650, "EA": 180, "Criticality": "Mandatory", "Equipment": "GT Enclosure, Vent Fans", "Remark": ""},
     {"System": "Demineralized Water (DW)", "Area": "Water Treatment", "Description": "Process water supply.", "DI": 2000, "EA": 480, "Criticality": "Highest", "Equipment": "DW Tank & Pump Station", "Remark": "Under review for sourcing from other power plant (by INTEGRA)"}
@@ -522,7 +522,7 @@ with tab_rep:
     | **1. Fuel Gas & FGH** | MB/GT/FGH | Supply Fuel Gas to GT | 2,480 | 610 | **Mandatory** | FGSS System, Performance Heater & Final Filter | |
     | **2. CCW** | MB/PR/PH1 | Supply & Return of CCW for equipment cooling down | 4,120 | 980 | **Mandatory** | PH#1 & FFC#1 Integrated | |
     | **3. IA** | All | Supply Instrument Air to all plant | 1,150 | 290 | **Operation** | Air Compressors, IA Dryers | |
-    | **4. AS/HW** | PR#3/4/AB | Anti-Icing path. | 2,650 | 660 | **Mandatory** | Aux. Boiler, Hot Water Supply Pump, Heat Exchanger | |
+    | **4. AS/HW** | PR#3/4/AB | supply & return of Hot Water for GT Anti-Icing | 2,650 | 660 | **Mandatory** | Aux. Boiler, Hot Water Supply Pump, Heat Exchanger | |
     | **5. N2** | GT / PR | Purging and inerting. | 320 | 90 | **Mandatory** | N2 Storage Tank, N2 Supply System | |
     | **6. GT MISC**| MB | Gas vent & drainage. | 650 | 180 | **Mandatory** | GT Enclosure, Vent Fans | |
     | **7. Demi. Water**| WT / PR | Process water supply. | 2,000 | 480 | **Highest** | DW Tank & Pump Station | **Under review for sourcing from other power plant (by INTEGRA)** |
